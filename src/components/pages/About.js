1e1ae97e18eb99fdf62f64f9913ab563aa19a8d2
Carli Hudson <carlihudson@Carlis-MacBook-Pro.local>
2023-03-12 22:04:46 -0700

    basic framework

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function About() {
+  return (
+    <div className="media container">
+  <div className="media-body row">
+    <h5 className="mt-0 mb-1">Welcome!</h5>
+    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+  </div>
+  <img class="ml-3" src='../../../public/assets/img/carli.jpg' alt="" />
+</div>
+  );
+}
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
+        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
+        dictum. 
+      </p>
+    </div>
+  );
+}
